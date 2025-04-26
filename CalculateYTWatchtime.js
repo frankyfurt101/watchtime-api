5826af424f24447475399c4dec3c11b5dc79cd09
Franky Fernandez <frankyfernandez@Frankys-MacBook-Pro.local>
2025-04-26 01:44:14 -0700

    Patch: Clean CalculateYTWatchtime.js for server sync

--- CalculateYTWatchtime.js
+++ CalculateYTWatchtime.js
@@ -4,5 +4,36 @@ const { parser } = pkg;
 import streamArrayPkg from 'stream-json/streamers/StreamArray.js';
 const { StreamArray } = streamArrayPkg;
 
-import { readFile } from 'fs/promises';
+import { createReadStream } from 'fs';
 import path from 'path';
+
+export async function processWatchHistory() {
+  return new Promise((resolve, reject) => {
+    const filePath = path.join('src', 'data', 'watch-history.json');
+    const jsonStream = StreamArray.withParser();
+
+    let totalSecondsWatched = 0;
+    let totalVideos = 0;
+
+    createReadStream(filePath)
+      .pipe(parser())
+      .pipe(jsonStream.input);
+
+    jsonStream.on('data', ({ value }) => {
+      if (value.hasOwnProperty('title')) {
+        totalVideos++;
+      }
+    });
+
+    jsonStream.on('end', () => {
+      resolve({
+        totalSecondsWatched,
+        totalVideos
+      });
+    });
+
+    jsonStream.on('error', (err) => {
+      reject(err);
+    });
+  });
+}
